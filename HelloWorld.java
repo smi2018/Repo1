4606236f42cca1b3cd9a39b5f240a887876822d0
smi2018 <pawarsmi25@gmail.com>
2018-08-29 16:28:09 +0530

    Add changes to java pgm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 class A {
  public static void main(String args[]){
-     System.out.println("Hello World");
+     for (int i=1;i<2;i++){
+        System.out.println("Hello World"+i);
+     }
  }
 }
